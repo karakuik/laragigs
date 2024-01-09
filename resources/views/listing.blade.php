0705441e38193e7f38856e133b85b94f4c30ed7d
karakuik <wesloke@gmail.com>
2024-01-08 19:48:05 -0500

    Added Images, Removed listing to refactor, modified User

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,6 +1,10 @@
+@extends('layout')
+
+@section('content')
 @if($listing == null)
     <p>No Listings Found</p>
 @else
     <h2>{{ $listing['title'] }}</h2>
     <p>{{ $listing['description'] }}</p>
 @endif
+@endsection
