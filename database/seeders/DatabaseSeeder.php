738af0db7c641ce79c95b3a9b4f0862819804acf
karakuik <wesloke@gmail.com>
2024-01-14 14:42:59 -0500

    Add hero blade for partial, data, listing and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,21 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use App\Models\User; //For User factory so no need for the ::
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @mixin Builder
+ */
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static Builder where($column, $operator = null, $value = null, $boolean = 'and')
+ * @method static Builder create(array $attributes = [])
+ * @method public Builder update(array $values)
+ */
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +26,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(10)->create();
+         User::factory(5)->create();
+
+//         Listing::create([
+//             'title' => 'Laravel Senior Developer',
+//             'tags' => 'laravel, javascript',
+//             'company' => 'Acme Corp',
+//             'location' => 'Boston, MA',
+//             'email' => 'email1@email.com',
+//             'website' => 'https://www.acme.com',
+//             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+//         ]);
+//
+//         Listing::create([
+//             'title' => 'Full-Stack Engineer',
+//             'tags' => 'laravel, backend ,api',
+//             'company' => 'Stark Industries',
+//             'location' => 'New York, NY',
+//             'email' => 'email2@email.com',
+//             'website' => 'https://www.starkindustries.com',
+//             'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+//         ]);
+
+        Listing::factory(6)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
