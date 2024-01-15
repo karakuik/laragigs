de006216a14485a70d7169bade6398c9d686ce7a
karakuik <wesloke@gmail.com>
2024-01-15 17:38:04 -0500

    Added retainment of create input even when field is empty or invalid

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -21,6 +21,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="company"
+                    value="{{old('company')}}"
                 />
                 @error('company')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -36,6 +37,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="title"
                     placeholder="Example: Senior Laravel Developer"
+                    value="{{old('title')}}"
                 />
                 @error('title')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -53,6 +55,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="location"
                     placeholder="Example: Remote, Boston MA, etc"
+                    value="{{old('location')}}"
                 />
                 @error('location')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -67,6 +70,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="email"
+                    value="{{old('email')}}"
                 />
                 @error('email')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -84,6 +88,7 @@
                     type="text"
                     class="border border-gray-200 rounded p-2 w-full"
                     name="website"
+                    value="{{old('website')}}"
                 />
                 @error('website')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -99,6 +104,7 @@
                     class="border border-gray-200 rounded p-2 w-full"
                     name="tags"
                     placeholder="Example: Laravel, Backend, Postgres, etc"
+                    value="{{old('tags')}}"
                 />
                 @error('tags')
                 <p class="text-red-500 text-xs mt-1">{{$message}}</p>
